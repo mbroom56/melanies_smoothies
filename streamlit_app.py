65fb6c631aa352d44d20d2431ad3157098559acb
mbroom56 <98361288+mbroom56@users.noreply.github.com>
2024-03-27 16:52:50 -0500

    Update streamlit_app.py
    Nutrition Information header
    pull accurate fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,8 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        st.subheader(fruit_chosen + 'Nutrition Information')
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         ingredients_string += fruit_chosen + ' '
 
